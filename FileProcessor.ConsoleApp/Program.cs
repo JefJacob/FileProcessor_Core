02de1bf0ba9752188efeabbde0051a1e45a808a5
jefjvincent <43353689+jefjvincent@users.noreply.github.com>
2020-03-14 04:26:44 -0400

    First

---
+++ FileProcessor.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NLog;
+using NLog.Config;
+
+namespace FileProcessor.ConsoleApp
+{
+    class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            //LogManager.Configuration = new XmlLoggingConfiguration($"{AppContext.BaseDirectory}nlog.config");
+            logger.Info("Application Started");
+            DataReader.ProcessFile(@"J:\Courses\Capstone\FileUpload\Source\InputFiles\GenericData-Athletics Ontario Outdoor Championship Series #3- U20 - Open Championships-12Jul2019-002.mdb");
+            logger.Info("Application Completed");
+        }
+    }
+}
